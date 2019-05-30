b887fc772accd094a86c4aca8628ec035c1e72c2
Margot Konig <margot.konig@blackrock.com>
2019-05-30 17:19:43 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,11 @@
+class Project
+  attr_reader :backers, :title
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << Backer.new(backer)
+  end
+end
